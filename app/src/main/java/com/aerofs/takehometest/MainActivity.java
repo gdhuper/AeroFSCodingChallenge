0f0910847b9d4d312cc511a483b308980a50ae1d
Gurpreet Singh <gdhuper@gmail.com>
2017-08-28 00:09:59 -0700

    mainactivity finised

--- app/src/main/java/com/aerofs/takehometest/MainActivity.java
+++ app/src/main/java/com/aerofs/takehometest/MainActivity.java
@@ -7,6 +7,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
 import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,6 +20,7 @@ import android.view.MenuItem;
 import android.view.ViewGroup;
 import android.view.ViewStub;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import java.util.*;
@@ -89,28 +92,58 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<L
         });
 
         //display Bio data for username
-        injectUserBio();
+       // injectUserBio();
 
     }
 
     public  void initLoader(){
         ConnectivityManager mgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
 
-        
+        NetworkInfo netInfo = mgr.getActiveNetworkInfo();
+
+        if(netInfo != null && netInfo.isConnected()){
+            //get loader manager reference
+            LoaderManager lmgr = getLoaderManager();
+
+            //initialize the loader with loader id
+            lmgr.initLoader(LOADER_ID, null, this);
+        }else{
+            View defaultView = findViewById(R.id.default_view);
+            defaultView.setVisibility(View.GONE);
+
+            defaultTextView.setText("No internet connection!");
+        }
     }
 
 
     /*
      * Helper method to add user bio layout to activity_main
      */
-    public void injectUserBio(){
+    public void injectUserBio(String avatar_url, String name, String userName, String location, String email, String blogUrl ){
 
         LayoutInflater vi = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View v = vi.inflate(R.layout.user_bio, null);
 
+        Uri avatar_uri = Uri.parse(avatar_url);
+        ImageView avatar = (ImageView) v.findViewById(R.id.avatar);
+        avatar.setImageURI(avatar_uri);
 
-        TextView textView = (TextView) v.findViewById(R.id.name);
-        textView.setText("Gurpreet Singh");
+
+        TextView nameView = (TextView) v.findViewById(R.id.name);
+        nameView.setText(name);
+
+        TextView userNameView = (TextView) v.findViewById(R.id.name);
+        userNameView.setText(userName);
+
+        TextView locationView = (TextView) v.findViewById(R.id.name);
+        locationView.setText(location);
+
+
+        TextView emailView = (TextView) v.findViewById(R.id.name);
+        emailView.setText(email);
+
+        TextView blogUrlView = (TextView) v.findViewById(R.id.name);
+        blogUrlView.setText(blogUrl);
 
 
         // insert into main view
@@ -119,8 +152,6 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<L
 
 
     }
-
-
 
     /*
      * Helper method to add action bar to top of main activity
@@ -169,16 +200,29 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<L
 
     @Override
     public Loader<List<RepoListItem>> onCreateLoader(int i, Bundle bundle) {
-        return null;
+        return new MainActivityLoader(this, REPOS_BASE_URL);
     }
 
     @Override
     public void onLoadFinished(Loader<List<RepoListItem>> loader, List<RepoListItem> repoListItems) {
+        View progress_bar = findViewById(R.id.progress_bar);
+        progress_bar.setVisibility(View.GONE);
+
+
+        defaultTextView.setText("No Repositories found!");
+
+        adapter.clear();
+
+        if(repoListItems != null && !repoListItems.isEmpty())
+        {
+            adapter.addAll(repoListItems);
+        }
 
     }
 
     @Override
     public void onLoaderReset(Loader<List<RepoListItem>> loader) {
-
+        //reset adapter to clear old data
+        adapter.clear();
     }
 }
